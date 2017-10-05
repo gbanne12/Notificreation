c33333f6d93139d4c5aceeb9f2534e0fb16e370b
gbanne12 <gbannerman14986@gmail.com>
2017-10-05 22:59:22 +0100

    Refactor the start command method into helper methods

--- app/src/main/java/com/bannerga/notificreation/notification/ServiceNotification.java
+++ app/src/main/java/com/bannerga/notificreation/notification/ServiceNotification.java
@@ -8,7 +8,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
@@ -20,10 +19,6 @@ import com.bannerga.notificreation.R;
 import java.util.Random;
 
 public class ServiceNotification extends Service {
-
-    NotificationManager notificationManager;
-    int id = 1;
-
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -32,61 +27,52 @@ public class ServiceNotification extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Random rand = new Random();
-        id  = Integer.parseInt(
-                Integer.toString(rand.nextInt(9) + 1)
-                        + Integer.toString(rand.nextInt(9) + 1)
-                        + Integer.toString(rand.nextInt(9) + 1));
-
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationContent content = NotificationContent.getInstance();
-
-        Intent notificationIntent = new Intent(this, DismissActivity.class);
-        notificationIntent.setData((Uri.parse("custom://"+System.currentTimeMillis())));
-
-        PendingIntent contentIntent = PendingIntent.getActivity(this, id++, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Build notification
-
+        NotificationContent content = NotificationContent.getInstance();
         Notification.Builder builder = new Notification.Builder(this)
-                .setContentIntent(contentIntent)
+                .setContentIntent(getPendingIntent())
                 .setContentTitle(content.getTitle())
                 .setContentText(content.getBody())
                 .setSmallIcon(R.mipmap.notification_icon)
                 .setColor(content.getColor());
-        setNotificationChannel(builder);
-        setColorizedForV26(builder);
+        setAndroidOBehaviour(manager, builder);
         Notification notification = builder.build();
 
         // Start foreground service
-        startForeground(id++, notification);
-
-        //notificationManager.notify(content.getId(), notification);
+        startForeground(getRandomDigits(), notification);
 
         return START_REDELIVER_INTENT;
     }
 
-
-    private boolean isOnAndroidO() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
-    }
-
-    private void setNotificationChannel(Notification.Builder builder) {
-        if (isOnAndroidO()) {
+    private void setAndroidOBehaviour(NotificationManager manager, Notification.Builder builder) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String CHANNEL_ID = "my_channel_01";
             NotificationChannel channel = new NotificationChannel(
                     "my_channel_01",
                     "Channel human readable title",
                     NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
+            manager.createNotificationChannel(channel);
             builder.setChannelId(CHANNEL_ID);
+            builder.setColorized(true);
         }
     }
 
-    private void setColorizedForV26(Notification.Builder builder) {
-        if (isOnAndroidO()) {
-            builder.setColorized(true);
-        }
+    private PendingIntent getPendingIntent() {
+        Intent notificationIntent = new Intent(this, DismissActivity.class);
+        notificationIntent.setData((Uri.parse("custom://" + System.currentTimeMillis())));
+
+        return PendingIntent.getActivity(this,
+                getRandomDigits(),
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private int getRandomDigits() {
+        Random rand = new Random();
+        return Integer.parseInt(Integer.toString(rand.nextInt(9) + 1)
+                        + Integer.toString(rand.nextInt(9) + 1)
+                        + Integer.toString(rand.nextInt(9) + 1));
     }
 }
